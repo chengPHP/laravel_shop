c36c2b75b57e8b6a60efb2075507f88a3e44d45d
1455394826@qq.com <1455394826@qq.com>
2019-03-11 13:25:39 +0800

    实现自定义用户表登录

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin','namespace' => 'Admin'],function ($router)
+{
+    $router->get('login', 'LoginController@showLogin')->name('admin.login');
+    $router->post('login', 'LoginController@login');
+    $router->post('logout', 'LoginController@logout');
+
+//    $router->get('index', 'AdminController@index');
+});
+//Route::group(['prefix' => 'admin','namespace' => 'Admin','middleware'=>'auth.admin'],function ($router)
+Route::group(['prefix' => 'admin','namespace' => 'Admin'],function ($router)
+{
+    $router->get('index', 'AdminController@index');
+    //后台用户管理
+    Route::resource('manager','ManagerController');
+    //角色管理
+    Route::resource('role','RoleController');
+    //权限管理
+    Route::resource('permission','PermissionController');
+});
