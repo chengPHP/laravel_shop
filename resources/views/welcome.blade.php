36cf5173456177da24d8e84e016d425039824539
1455394826@qq.com <1455394826@qq.com>
2019-03-11 11:13:48 +0800

    dev项目初始化

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel项目初始化
                 </div>
 
                 <div class="links">
